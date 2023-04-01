2594eb5b38b6a44990d096bfba6f7737b8ea87b1
andynorris <47344131+irvineAlgotrading@users.noreply.github.com>
2023-04-01 11:10:26 -0700

    Fixes

--- april1.py
+++ april1.py
@@ -1,8 +1,10 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
-def world_environment(value):
-    return random.uniform(-1, 1)
+def world_environment(value, iteration):
+    randomness = 1 - (iteration * 0.01)
+    return random.uniform(-1 * randomness, randomness)
 
 def rebalance_weights(weights):
     min_weight = min(weights)
@@ -14,14 +16,31 @@ def main():
     weights = np.random.uniform(-5, 5, 20)
     print(f"Original weights: {weights}")
 
-    rebalanced_weights = rebalance_weights(weights)
-    print(f"Rebalanced weights: {rebalanced_weights}")
+    # Initialize lists for storing weights and iteration numbers for the plot
+    plot_weights = []
+    plot_iterations = []
 
-    estimated_output = sum(rebalanced_weights) / len(rebalanced_weights)
-    print(f"Estimated output: {estimated_output}")
+    for iteration in range(100):
+        rebalanced_weights = rebalance_weights(weights)
+        estimated_output = sum(rebalanced_weights) / len(rebalanced_weights)
 
-    response = world_environment(estimated_output)
-    print(f"World environment response: {response}")
+        response = world_environment(estimated_output, iteration)
+
+        # Update weights based on the environment's response
+        weights = [w + response for w in rebalanced_weights]
+
+        plot_weights.append(weights)
+        plot_iterations.append(iteration)
+
+    print(f"Final weights: {weights}")
+
+    # Create the plot
+    plt.figure()
+    plt.plot(plot_iterations, plot_weights)
+    plt.xlabel("Iteration")
+    plt.ylabel("Weights")
+    plt.title("Weights Stabilization as Randomness Decreases")
+    plt.show()
 
 if __name__ == "__main__":
     main()
