03f31c90d9d7424d62c0ddbac89b744c78e79ed9
andynorris <47344131+irvineAlgotrading@users.noreply.github.com>
2023-04-01 11:56:33 -0700

    Fixes

--- april1.py
+++ april1.py
@@ -2,9 +2,9 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-def world_environment(value, iteration):
-    randomness = 1 - (iteration * 0.01)
-    return random.uniform(-1 * randomness, randomness)
+def world_environment(values, iteration):
+    randomness = 1 - np.log10(iteration + 1) / 3.33
+    return [random.uniform(-1 * randomness, randomness) for _ in values]
 
 def rebalance_weights(weights):
     min_weight = min(weights)
@@ -13,6 +13,7 @@ def rebalance_weights(weights):
     return rebalanced_weights
 
 def main():
+    #these are the things it will be optimizing for
     self_awareness_aspects = [
         'body_awareness',
         'emotional_awareness',
@@ -65,36 +66,56 @@ def main():
     increment = 2 / (num_aspects - 1)
     initial_values = {aspect: -1 + increment * (rank - 1) for rank, aspect in consciousness_rank.items()}
     weight_dict = {aspect: initial_values[aspect] for aspect in self_awareness_aspects}
-    print(f"Initial weights: {weight_dict}")
+
+    print("Initial Weights:")
+    print(f"Aspect{' ':<20}Weight")
+    for aspect, weight in sorted(weight_dict.items(), key=lambda x: x[1], reverse=True):
+        print(f"{aspect:<24}{weight:.4f}")
 
     plot_weights = []
     plot_iterations = []
+    plot_randomness = []
 
-    for iteration in range(100):
+    for iteration in range(100000):
         rebalanced_weights = rebalance_weights(list(weight_dict.values()))
         weight_dict = dict(zip(self_awareness_aspects, rebalanced_weights))
         estimated_output = sum(rebalanced_weights) / len(rebalanced_weights)
 
-        response = world_environment(estimated_output, iteration)
+        responses = world_environment(values=rebalanced_weights, iteration=iteration)
 
         # Update weights based on the environment's response
-        updated_weights = [w + response for w in rebalanced_weights]
+        updated_weights = [w + response for w, response in zip(rebalanced_weights, responses)]
         weight_dict = dict(zip(self_awareness_aspects, updated_weights))
 
         plot_weights.append(updated_weights)
         plot_iterations.append(iteration)
+        plot_randomness.append(1 - np.log10(iteration + 1) / 3.33)
 
-    print(f"Final weights: {weight_dict}")
+    print("\nFinal Weights:")
+    print(f"Aspect{' ':<20}Weight")
+    for aspect, weight in sorted(weight_dict.items(), key=lambda x: x[1], reverse=True):
+        print(f"{aspect:<24}{weight:.4f}")
 
     # Create the plot
-    plt.figure()
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8), sharex=True)
+
+    # Plot the weight stabilization
     for i, aspect in enumerate(self_awareness_aspects):
-        plt.plot(plot_iterations, [w[i] for w in plot_weights], label=aspect)
-    plt.xlabel("Iteration")
-    plt.ylabel("Weights")
-    plt.title("Weights Stabilization as Randomness Decreases")
-    plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+        ax1.plot(plot_iterations, [w[i] for w in plot_weights], label=aspect)
+    ax1.set_ylabel("Weights")
+    ax1.set_title("Weights Stabilization as Randomness Decreases")
+    ax1.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+
+    # Plot the degradation of the randomness function
+    ax2.plot(plot_iterations, plot_randomness)
+    ax2.set_ylabel("Randomness")
+    ax2.set_xlabel("Iteration")
+
+    # Set the chart settings
+    plt.subplots_adjust(left=0.065, bottom=0.125, right=0.802, top=0.88, wspace=0.2, hspace=0.2)
+
     plt.show()
+
 
 if __name__ == "__main__":
     main()
